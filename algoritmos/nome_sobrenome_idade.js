4d2e411ec99bb6930f9359fe23b72ea1f1ac46f6
Ulisses Hen <ulisses.hen@hotmail.com>
2022-03-03 16:19:38 -0300

    adicionado imagem; removido palavra log do comentario;

--- algoritmos/nome_sobrenome_idade.js
+++ algoritmos/nome_sobrenome_idade.js
@@ -3,4 +3,4 @@ var sobrenome = 'Hen';
 var idade = 29;
 
 console.log('Meu nome é ' + nome + ' ' + sobrenome + ' e tenho ' + idade + ' anos.');
-// log >> Meu nome é Ulisses Hen e tenho 29 anos.
+// Meu nome é Ulisses Hen e tenho 29 anos.
