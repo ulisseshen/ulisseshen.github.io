d693c2e24f9492403de29924b2765422402267c2
Ulisses Hen <ulisses.hen@hotmail.com>
2022-03-03 16:41:49 -0300

    adicionar algoritmo em c#

---
+++ algoritmos/nome_sobrenome_idade.cs
@@ -0,0 +1,6 @@
+var nome = "Ulisses";
+var sobrenome = "Hen";
+var idade = 29;
+
+Console.WriteLine("Meu nome é " + nome + " " + sobrenome + " e tenho " + idade + " anos.");
+// Meu  nome é Ulisses Hen e tenho 29 anos.
